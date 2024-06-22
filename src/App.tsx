d549de519dee7975b0de7f1d60585f16c57d5324
ThangNP <phuocthang238@gmail.com>
2024-06-22 13:23:55 +0700

    react form

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import FormComponent from "./components/Form";
+
+function App() {
+  return (
+    <>
+      <div>
+        <FormComponent />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Form.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+
+let renderCount = 0;
+const FormComponent = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    channel: "",
+  });
+
+  renderCount++;
+  const { register, control, handleSubmit } = useForm();
+  const onSubmit = (data: any) => {
+    console.log("daaa", data);
+  };
+  // đây là các value có trong register
+  // const { name, ref, onChange, onBlur } = register("username");
+  return (
+    <div>
+      <h1>YouTube Form {renderCount / 2}</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="username">Username</label>
+          {/* <input type="text" id="username" name="username" /> */}
+          <input type="text" id="username" {...register("username")} />
+        </div>
+
+        <div>
+          <label htmlFor="email">E-mail</label>
+          <input type="email" id="email" {...register("email")} />
+        </div>
+
+        <div>
+          <label htmlFor="channel">Channel</label>
+          <input type="text" id="channel" {...register("channel")} />
+        </div>
+
+        <div>
+          <button>Submit</button>
+        </div>
+      </form>
+      <DevTool control={control} />
+    </div>
+  );
+};
+
+export default FormComponent;
